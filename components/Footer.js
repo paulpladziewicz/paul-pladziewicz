8133778a5b91ed6c700cb85ee24c6c9d9cb21919
Paul Pladziewicz <41588646+paulpladziewicz@users.noreply.github.com>
2021-05-03 19:54:03 -0400

    basic footer structure added

---
+++ components/Footer.js
@@ -0,0 +1,37 @@
+import Link from 'next/link'
+
+export default function Footer() {
+  return (
+    <footer className='mt-20 flex'>
+
+      <div className='w-1/2'>
+        <h2>Think I could help?</h2>
+        <p><span>Reach out.</span> I would be happy to have a discussion.</p>
+      </div>
+
+      <div className='w-1/2 bg-gray-50'>
+        <form className='flex flex-wrap justify-between' action="">
+          <div style={{width:'49%'}}>
+            <input className='w-full p-4 rounded-2xl bg-gray-100 text-lg' type="text" placeholder='Name'/>
+          </div>
+          <div style={{width:'49%'}}>
+            <input className='w-full p-4 rounded-2xl bg-gray-100 text-lg' type="text" placeholder='Business'/>
+          </div>
+          <div className='mt-4'style={{width:'49%'}}>
+            <input className='w-full p-4 rounded-2xl bg-gray-100 text-lg' type="email" placeholder='Email' required={true}/>
+          </div>
+          <div className='mt-4' style={{width:'49%'}}>
+            <input className='w-full p-4 rounded-2xl bg-gray-100 text-lg' type="text" placeholder='How did you find me?' required={true}/>
+          </div>
+          <div className='w-full mt-4'>
+            <textarea className='w-full p-4 rounded-2xl bg-gray-100 text-lg' name="message" id="message" placeholder='How can I assist?' rows="5" required={true}/>
+          </div>
+          <div>
+            <input type="submit"/>
+          </div>
+        </form>
+      </div>
+
+    </footer>
+  )
+}
--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,12 @@
 import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 export default function Layout({ children }) {
   return (
     <div className="max-w-screen-xl mx-auto">
       <Navbar />
       {children}
+      <Footer />
     </div>
   );
 }

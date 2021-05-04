78a90ac8d6e631dadfd16594b0a61f51b1695c92
Paul Pladziewicz <41588646+paulpladziewicz@users.noreply.github.com>
2021-05-03 20:58:42 -0400

    implemented internationalization basics and added basic intl structure to the Navbar component

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,14 +1,43 @@
+import { useRouter } from "next/router";
 import Link from 'next/link'
 
+const en = {
+  about: 'About',
+};
+const es = {
+  about: 'Sobre'
+};
+
 export default function Navbar() {
+  const router = useRouter();
+  const { locale } = router;
+  const t = locale === "en" ? en : es;
+
+  const changeLanguage = (e) => {
+    const locale = e.target.value;
+    router.push(router.pathname, router.asPath, { locale });
+  };
+
   return (
     <nav className='flex justify-between'>
       <span>Paul</span>
       <ul>
-        <Link href="/about"><a>About</a></Link>
+        <Link href="/about"><a>{t.about}</a></Link>
         <Link href="/work"><a>Work</a></Link>
         <Link href="/opinion"><a>Opinion</a></Link>
         <Link href="/contact"><a>Contact</a></Link>
+        <select
+          onChange={changeLanguage}
+          defaultValue={locale}
+          className="text-xl p-1 border-2 rounded-md"
+        >
+          <option className="text-gray-900" value="en">
+            English
+          </option>
+          <option className="text-gray-900" value="es">
+            Español
+          </option>
+        </select>
       </ul>
     </nav>
   )

e33e97925be289111587017fa2d028fe03cd7161
Paul Pladziewicz <41588646+paulpladziewicz@users.noreply.github.com>
2021-06-13 13:38:03 -0400

    fixed react-icons bug

--- components/MobileMenu.js
+++ components/MobileMenu.js
@@ -1,4 +1,4 @@
-// import {FaCompressAlt, FaExpandAlt} from "react-icons/all";
+import {FaCompressAlt, FaExpandAlt} from "react-icons/fa";
 import {useReducer} from "react";
 
 export default function MobileMenu() {
@@ -7,7 +7,7 @@ export default function MobileMenu() {
     <div className='sm:hidden fixed bottom-4 right-4 rounded-full p-4 bg-purple-700 text-white cursor-pointer'
          onClick={toggleMenu}
     >
-      {/*{menu ? <FaCompressAlt size={20}/> : <FaExpandAlt size={20}/>}*/}
+      {menu ? <FaCompressAlt size={20}/> : <FaExpandAlt size={20}/>}
     </div>
   );
 }
